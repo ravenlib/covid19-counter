61b58244c402a755fd350be74c5273a91d846677
Jerry Wang <jerryw4113@gmail.com>
2020-03-19 19:56:32 +1000

    Added handling of downstream exception

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import styled from 'styled-components';
 import TotalStatBody from './TotalStatBody';
 import axios from 'axios';
 import Spinner from 'react-bootstrap/Spinner'
+import Alert from 'react-bootstrap/Alert'
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
 export default class App extends React.Component {
 
   state = {
+    isFailed: false,
     isLoading: true,
     countriesStat: []
   }
   async componentDidMount() {
-    const totalStat = await (await axios.get('https://corona.lmao.ninja/all')).data;
-    const countriesStat = await (await axios.get('https://corona.lmao.ninja/countries')).data;
-    this.setState({
-      isLoading: false,
-      totalStat,
-      countriesStat
-    })
+    try {
+      const totalStat = await (await axios.get('https://covid19-counter-api.herokuapp.com/v1/statistics/summary')).data;
+      const countriesStat = await (await axios.get('https://corona.lmao.ninja/countries')).data;
+      this.setState({
+        isLoading: false,
+        totalStat,
+        countriesStat
+      })
+    } catch(err) {
+      this.setState({
+        isFailed: true
+      })
+    }
   }
 
   render() {
@@ -27,32 +35,35 @@ export default class App extends React.Component {
       console.log(new Date(this.state.totalStat.updated));
     }
     return (
-      <MainWrapper>
-        <hr />
-        <TitleWrapper>
-          <h1>COVID-19 Counter</h1>
-        </TitleWrapper>
-        <hr />
-        <TotalStatBody isLoading={this.state.isLoading} totalStat={this.state.totalStat} />
-        <hr />
-        {this.state.isLoading ? <Spinner animation='grow' role="status" size='xxl'><span className='sr-only'>Loading...</span></Spinner>
-          : <BootstrapTable data={this.state.countriesStat} search={ true } striped hover>
-              <TableHeaderColumn dataField='country' isKey headerAlign='center' dataAlign='center' dataSort={ true }>Country</TableHeaderColumn>
-              <TableHeaderColumn dataField='todayCases' headerAlign='center' dataAlign='right' dataSort={ true }>New Cases</TableHeaderColumn>
-              <TableHeaderColumn dataField='todayDeaths' headerAlign='center' dataAlign='right' dataSort={ true }>New Deaths</TableHeaderColumn>
-              <TableHeaderColumn dataField='critical' headerAlign='center' dataAlign='right' dataSort={ true }>Critical</TableHeaderColumn>
-              <TableHeaderColumn dataField='cases' headerAlign='center' dataAlign='right' dataSort={ true }>Total Cases</TableHeaderColumn>
-              <TableHeaderColumn dataField='deaths' headerAlign='center' dataAlign='right' dataSort={ true }>Total Deaths</TableHeaderColumn>
-              <TableHeaderColumn dataField='recovered' headerAlign='center' dataAlign='right' dataSort={ true }>Total Recovered</TableHeaderColumn>
-            </BootstrapTable>
-        }
-      </MainWrapper>
+      <div>
+        {this.state.isFailed && <Alert variant="danger">Error has occurred, please refresh the page or contact the administrator</Alert>}
+        <MainWrapper>
+          <hr />
+          <TitleWrapper>
+            <h1>COVID-19 Counter</h1>
+          </TitleWrapper>
+          <hr />
+          <TotalStatBody isLoading={this.state.isLoading} totalStat={this.state.totalStat} />
+          <hr />
+          {this.state.isLoading ? <Spinner animation='grow' role="status" size='xxl'><span className='sr-only'>Loading...</span></Spinner>
+            : <BootstrapTable data={this.state.countriesStat} search={ true } striped hover>
+                <TableHeaderColumn dataField='country' isKey headerAlign='center' dataAlign='center' dataSort={ true }>Country</TableHeaderColumn>
+                <TableHeaderColumn dataField='todayCases' headerAlign='center' dataAlign='right' dataSort={ true }>New Cases</TableHeaderColumn>
+                <TableHeaderColumn dataField='todayDeaths' headerAlign='center' dataAlign='right' dataSort={ true }>New Deaths</TableHeaderColumn>
+                <TableHeaderColumn dataField='critical' headerAlign='center' dataAlign='right' dataSort={ true }>Critical</TableHeaderColumn>
+                <TableHeaderColumn dataField='cases' headerAlign='center' dataAlign='right' dataSort={ true }>Total Cases</TableHeaderColumn>
+                <TableHeaderColumn dataField='deaths' headerAlign='center' dataAlign='right' dataSort={ true }>Total Deaths</TableHeaderColumn>
+                <TableHeaderColumn dataField='recovered' headerAlign='center' dataAlign='right' dataSort={ true }>Total Recovered</TableHeaderColumn>
+              </BootstrapTable>
+          }
+        </MainWrapper>
+      </div>
     );
   }
 }
 
 const MainWrapper = styled.section`
-  width: 80%;
+  width: 90%;
   text-align: center;
   margin: auto;
 `;
